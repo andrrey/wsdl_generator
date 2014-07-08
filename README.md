c8359557f044be23b5076ec324165cfbd1c1b208
andrrey <andygluk@ya.ru>
2014-07-08 12:24:36 +0400

    ver 1.1: added useful options

--- README.md
+++ README.md
@@ -6,3 +6,19 @@ This script will generate WSDL 1.1 service containing all tags needed, you just 
 It is very useful when you designing new system and need to create a lot of WSDLs. You may just generate them with this script, and then complete them manually or import them into an application like Enterprise Architect.
 Generated WSDL will contain one service with as much operations as you specify in command line.
 All operations are request-reply wrapped document/literal.
+Generated WSDL will be output to screen, so you have to > it to some file.
+
+Usage:
+`wsdl_generator.pl [options] <basename> <targetNamespace> [<operation1> <operation2> <operation3> ...]`
+`<basename>` is a name which will be used as a base for generating all other names. So servicese will be called `<basename>Service`, binding `<basename>Binding`, operations will be `<operation1><basename>`, etc.
+`<targetnamespace>` is a namespace used in elements of generated WSDL
+If operation list is omitted, then it defaults to one operation named "Get"
+
+Options:
+-v or --verbose be verbose. All messages will be written as HTML comments, so if you redirected output in file, it is safe.
+--nofaults do not generate fault contracts
+--exclude_basename do not complete operation names with basename, so if you specified operation Put in command line it will be called Put in WSDL, insted of Put<basename>
+
+Example:
+`wsdl_generator.pl Test http://test.localhost/`
+Will generate service called TestService in namespace http://test.localhost/ located at http://test.localhost/TestServiceю It will have one operation named GetTest
--- wsdl_generator.pl
+++ wsdl_generator.pl
@@ -1,24 +1,44 @@
 #!/usr/bin/perl
 #
+$version_major = 1;
+$version_minor = 1;
+#
 # (c) Andrey Semenyuk 2014
 # see http://github.com/andrrey/wsdl_generator
 # This script will generate WSDL 1.1 service containing all tags needed.
 # You just have to propertly fill all types with fields required by your service. 
 #
 
-sub help_banner{
-	print "This script will generate WSDL 1.1 service containing all tags needed.\n";
-	print "You just have to propertly complete all types with fields required by your service.\n";
-	print "Usage:\t$0 <basename> <targetNamespace> [<operation1> <operation2> <operation3> ...]\n";
-	print "If operation list is omitted, then it defaults to one operation named \"Get\"\n";
-	print "Example:\t$0 Test http://test.localhost/\n";
-	print "\tWill generate service called TestService in namespace http://test.localhost/ located at http://test.localhost/TestService\n";
-	die "\n";
+use Getopt::Long;
+
+sub version_banner{
+    print "WSDL Generator version $version_major.$version_minor\n";
 }
+
+sub help_banner{
+    version_banner;
+	print "This script will generate WSDL 1.1 service containing all tags needed.\n";
+	print "You just have to propertly complete all generated types with fields\nrequired by your service.\n";
+	print "Usage:\t$0 [options] <basename> <targetNamespace> [<operation1> <operation2> <operation3> ...]\n";
+	print "If operation list is omitted, then it defaults to one operation named \"Get\"\n";
+    print "\nOptions:\n\t-v or --verbose\tbe verbose. All messages will be written as HTML comments\n";
+    print "\t--nofaults\tdo not generate fault contracts\n";
+    print "\t--exclude_basename\tdo not complete operation names with basename\n";
+	print "\nExample:\t$0 Test http://test.localhost/\n";
+	print "\tWill generate service called TestService in namespace http://test.localhost/ located at http://test.localhost/TestService\n";
+    print "\tIt will have one operation named GetTest";
+	die("\n");
+}
+
+$verbose = 0;
+$faults = 1;
+$exclude_basename = 0;
+
+Getopt::Long::Configure ('bundling');
+GetOptions('v|verbose' => \$verbose, 'faults!' => \$faults, 'exclude_basename' => \$exclude_basename);
 
 $basename = shift or help_banner;
 $targetNamespace = shift or help_banner;
-
 #target namespace must be ended with / char.
 if(substr($targetNamespace, -1, 1) ne "/") {$targetNamespace = $targetNamespace . "/";}
 
@@ -40,7 +60,9 @@ $portTypeName = $portName . "Type";
 
 foreach $operation (@ops){
 	my %hash;
-	$operationName = $operation . $basename;
+    if($verbose) {print "<!-- Operation $operation -->\n";}
+    if($exclude_basename) {$operationName = $operation;}
+	else {$operationName = $operation . $basename;}
 	$hash{operationName} = $operationName;
 	$hash{operationURI} = $targetNamespace . "actions/" . $operationName;
 	$hash{operationInputName}= $operationName . "Req";
@@ -62,7 +84,7 @@ foreach $operation (@ops){
 }
 
 print <<TEMPLATE_START_WSDL;
-<?xml version="1.0" encoding="windows-1252"?>
+<?xml version="1.0"?>
 <!-- Generated with WSDL Generator, see http://github.com/andrrey/wsdl_generator -->
 <wsdl:definitions name="$wsdlDefinitionsName" targetNamespace="$targetNamespace"
 	xmlns:tns="$targetNamespace"
@@ -83,13 +105,16 @@ for $i (0 .. $#data){
     		</xs:complexType>
     		<xs:complexType name="$data[$i]{operationOutputElemType}">
     		</xs:complexType>
-    		<xs:complexType name="$data[$i]{operationFaultElemType}">
-    		</xs:complexType>
     		<xs:element name="$data[$i]{operationInputElement}" type="s1:$data[$i]{operationInputElemType}"/>
     		<xs:element name="$data[$i]{operationOutputElement}" type="s1:$data[$i]{operationOutputElemType}"/>
-    		<xs:element name="$data[$i]{operationFaultElement}" type="s1:$data[$i]{operationFaultElemType}"/>
 TEMPLATE_TYPES
+    $here_fault = <<TEMPLATE_FAULT_TYPES;
+            <xs:complexType name="$data[$i]{operationFaultElemType}">
+            </xs:complexType>
+            <xs:element name="$data[$i]{operationFaultElement}" type="s1:$data[$i]{operationFaultElemType}"/>
+TEMPLATE_FAULT_TYPES
 	print $here . "\n";
+    if($faults) {print $here_fault . "\n";}
 }
 
 print "   	</xs:schema>\n\t</wsdl:types>\n";
@@ -102,11 +127,14 @@ for $i (0 .. $#data){
     <wsdl:message name="$data[$i]{operationOutputMsgName}">
     	<wsdl:part name="body" element="tns:$data[$i]{operationOutputElement}"/>
     </wsdl:message>
-    <wsdl:message name="$data[$i]{operationFaultMsgName}">
-    	<wsdl:part name="body" element="tns:$data[$i]{operationFaultElement}"/>
-    </wsdl:message>
 TEMPLATE_MESSAGES
+    $here_fault = <<TEMPLATE_FAULT_MESSAGES;
+    <wsdl:message name="$data[$i]{operationFaultMsgName}">
+        <wsdl:part name="body" element="tns:$data[$i]{operationFaultElement}"/>
+    </wsdl:message>
+TEMPLATE_FAULT_MESSAGES
 	print $here . "\n";
+    if($faults) {print $here_fault . "\n";}
 }
 
 print"    <wsdl:portType name=\"$portTypeName\">\n";
@@ -116,10 +144,13 @@ for $i (0 .. $#data){
     	<wsdl:operation name="$data[$i]{operationName}">
     		<wsdl:input name="$data[$i]{operationInputName}" message="tns:$data[$i]{operationInputMsgName}"/>
     		<wsdl:output name="$data[$i]{operationOutputName}" message="tns:$data[$i]{operationOutputMsgName}"/>
-    		<wsdl:fault name="$data[$i]{operationFaultName}" message="tns:$data[$i]{operationFaultMsgName}"/>
-    	</wsdl:operation>
 TEMPLATE_OPERATIONS
+    $here_fault = <<TEMPLATE_FAULT_OPERATIONS;
+            <wsdl:fault name="$data[$i]{operationFaultName}" message="tns:$data[$i]{operationFaultMsgName}"/>
+TEMPLATE_FAULT_OPERATIONS
 	print $here . "\n";
+    if($faults) {print $here_fault . "\n";}
+    print "     </wsdl:operation>\n";
 }
 
 print <<TEMPLATE_PORT;
@@ -139,12 +170,15 @@ for $i (0 .. $#data){
             <wsdl:output name="$data[$i]{operationOutputName}">
                 <soap:body use="literal"/>
             </wsdl:output>
+TEMPLATE_PORT_OPS
+    $here_fault = <<TEMPLATE_PORT_FAULT_OPS;
             <wsdl:fault name="$data[$i]{operationFaultName}">
                 <soap:fault name="$data[$i]{operationFaultName}" use="literal"/>
-            </wsdl:fault>
-        </wsdl:operation>
-TEMPLATE_PORT_OPS
+            </wsdl:fault>    
+TEMPLATE_PORT_FAULT_OPS
 	print $here . "\n";
+    if($faults) {print $here_fault . "\n";}
+    print "        </wsdl:operation>\n";
 }
 
 print <<TEMPLATE_END_WSDL;
